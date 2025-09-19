26a28089b4e6cee672fd84ee60d417eddae9e956
Shoaib khan <shoaibmangal@yahoo.com>
2025-09-19 23:50:24 +0500

    error resolve

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-import "./styles/custom.css";
+import "./custom.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 

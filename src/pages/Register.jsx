398560f7ff1d0b0fb1c0b78dc3fce1368740aab9
Shoaib khan <shoaibmangal@yahoo.com>
2025-09-20 00:24:12 +0500

    error resolve

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./register.css";
+import "./Register.css";
 function Register() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");

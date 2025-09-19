d6325ed9683214e4585534a5d9fa5504bd204147
Shoaib khan <shoaibmangal@yahoo.com>
2025-09-19 23:54:29 +0500

    error resolve

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./login.css";
 
 function Login() {
   const [email, setEmail] = useState("");
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./register.css";
 
 function Register() {
   const [name, setName] = useState("");
